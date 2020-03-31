d26fc15924a723156595ed2736e11cbef223e47e
Wellington <wellsousa1987@outlook.com>
2020-03-30 21:53:25 -0300

    feat: garante que SignUpController retorne um erro se nenhum email for fornecido

--- src/presentation/controllers/SignUpController.js
+++ src/presentation/controllers/SignUpController.js
@@ -1,10 +1,24 @@
 
 
 SignUpController = {
-    handle: function(httpRequest){
-        return {
-            statusCode: 400,
-            body: new Error('Missing param: name')
+    handle: function( httpRequest ){
+       
+        if(! httpRequest.body.name){
+            return {
+                statusCode: 400,
+                body: new Error('Missing param: name')
+            }
+        }
+
+        if(! httpRequest.body.email){
+            return{
+                statusCode: 400,
+                body: new Error('Missing param: email')
+            }
+        }
+
+        return{
+            statusCode: 400
         }
     }
 }
